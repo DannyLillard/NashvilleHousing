9087dc5f603ac346c3d4090494bda44ef5a5998a
Danny Lillard <dannylillard10@gmail.com>
2021-06-08 11:31:56 -0500

    Added the SQL files, just starting out.

---
+++ NashvilleHousing/Database/nashvilleDataCleaning.sql
@@ -0,0 +1,4 @@
+/*  Author: Danny Lillard
+ *  Date: 6/7/2021
+ *  Desc: File containing the data cleaning queries for the Nashville housing dataset.
+ */
---
+++ NashvilleHousing/Database/nashvilleDatabaseCreation.sql
@@ -0,0 +1,6 @@
+/*  Author: Danny Lillard
+ *  Date: 6/7/2021
+ *  Desc: Database description for the nashville housing dataset.
+ */
+
+ 
